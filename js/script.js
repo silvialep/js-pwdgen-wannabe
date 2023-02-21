77c5942f633375181d42a1d0d90a0073adf9e581
silvialep <silvia.lepore_sl@libero.it>
2023-02-21 13:53:35 +0100

    feat: files created

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
+// (es: MarioRossiVerde21) (modificato) 
+
