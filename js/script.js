ffc2951766ab03a56b734e7ac3b8dc848227500e
silvialep <silvia.lepore_sl@libero.it>
2023-02-21 14:00:49 +0100

    feat: ex done

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@
 // Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
 // (es: MarioRossiVerde21) (modificato) 
 
+let userName = prompt(`Ciao, qual è il tuo nome?`);
+let userSurname = prompt(`Ciao ${userName}, e qual è il tuo cognome?`);
+let userColor = prompt(`Benvenuto/a ${userName} ${userSurname}, qual è il tuo colore preferito?`);
+document.writeln(userName + userSurname + userColor + 88);
